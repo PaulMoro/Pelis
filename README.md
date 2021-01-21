d067c345cd9a7097f7442e22e0f8d5c3fb8a8d7f
Paul Montoya <paulmontoya15@gmail.com>
2021-01-20 23:10:59 -0500

    Bootstrap has problems, first component and styles

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Wireframe 
+[Here](https://www.figma.com/file/c5TcJbdLRtCLqHxwM0sOao/Untitled?node-id=0%3A1)
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
---
+++ styles/components/header.js
@@ -0,0 +1,26 @@
+import css from "styled-jsx/css";
+
+export default css.HeaderStyle`
+.header { 
+  width: 100%;
+  height: 100px;
+
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+
+  background-color: #ffd369;
+  align-items: center;
+  color: #222831;
+}
+
+.header_icon {
+  margin-left: 10px
+}
+
+.buttonForm {
+
+}
+
+
+`;
